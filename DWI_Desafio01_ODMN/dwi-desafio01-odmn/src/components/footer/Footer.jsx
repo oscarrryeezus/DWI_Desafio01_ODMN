81e108e0a4203e6a987c7f66d5d0b2dd19372cf8
oscarrryeezus <oscamn2869@gmail.com>
2025-06-09 20:20:14 -0600

    lol

--- DWI_Desafio01_ODMN/dwi-desafio01-odmn/src/components/footer/Footer.jsx
+++ DWI_Desafio01_ODMN/dwi-desafio01-odmn/src/components/footer/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export function Footer() {
   return (
     <footer>
-      <p>© 2025 Mi Aplicación. Todos los derechos reservados.</p>
+      <p>
+        © 2025 Mi Aplicación. Todos los derechos reservados.
+      </p>
     </footer>
   );
 }
